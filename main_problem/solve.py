a31b31027021dbfdcdabea3d42bcd039b4132f2f
Timothée Lottaz <timozattol@gmail.com>
2018-03-01 19:18:57 +0100

    Empty cars, ready to ride :)

--- main_problem/solve.py
+++ main_problem/solve.py
@@ -8,8 +8,11 @@ R, C, F, N, B, T = [int(x) for x in (lines[0].split())]
 
 rides = [tuple(map(int, line.split())) for line in lines[1:]]
 
+cars = [([], (0, 0), 0) for c in range(F)]
 
 
 print(R, C, F, N, B, T)
 
 print(rides)
+
+print(cars)
