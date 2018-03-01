88f06d25506b3e27217aae7194871e9372756ff2
Timothée Lottaz <timozattol@gmail.com>
2018-03-01 19:16:36 +0100

    Parse params & rides

--- main_problem/solve.py
+++ main_problem/solve.py
@@ -3,3 +3,13 @@ import numpy as np
 
 lines = sys.stdin.readlines()
 lines = [line.strip() for line in lines]
+
+R, C, F, N, B, T = [int(x) for x in (lines[0].split())]
+
+rides = [tuple(map(int, line.split())) for line in lines[1:]]
+
+
+
+print(R, C, F, N, B, T)
+
+print(rides)
